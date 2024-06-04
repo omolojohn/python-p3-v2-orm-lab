a2426b6d2faccc1944886a9729e45c9acfe2c3dc
omolo-j <omoloj135@gmail.com>
2024-06-04 20:49:52 +0300

    v2-orm-lab

--- lib/employee.py
+++ lib/employee.py
@@ -1,10 +1,7 @@
-# lib/employee.py
-from __init__ import CURSOR, CONN
+from __init__ import CONN, CURSOR
 from department import Department
 
 class Employee:
-
-    # Dictionary of objects saved to the database.
     all = {}
 
     def __init__(self, name, job_title, department_id, id=None):
@@ -28,9 +25,7 @@ class Employee:
         if isinstance(name, str) and len(name):
             self._name = name
         else:
-            raise ValueError(
-                "Name must be a non-empty string"
-            )
+            raise ValueError("Name must be a non-empty string")
 
     @property
     def job_title(self):
@@ -41,9 +36,7 @@ class Employee:
         if isinstance(job_title, str) and len(job_title):
             self._job_title = job_title
         else:
-            raise ValueError(
-                "job_title must be a non-empty string"
-            )
+            raise ValueError("Job title must be a non-empty string")
 
     @property
     def department_id(self):
@@ -54,8 +47,7 @@ class Employee:
         if type(department_id) is int and Department.find_by_id(department_id):
             self._department_id = department_id
         else:
-            raise ValueError(
-                "department_id must reference a department in the database")
+            raise ValueError("Department ID must reference a department in the database")
 
     @classmethod
     def create_table(cls):
@@ -88,10 +80,8 @@ class Employee:
                 INSERT INTO employees (name, job_title, department_id)
                 VALUES (?, ?, ?)
         """
-
         CURSOR.execute(sql, (self.name, self.job_title, self.department_id))
         CONN.commit()
-
         self.id = CURSOR.lastrowid
         type(self).all[self.id] = self
 
@@ -102,26 +92,19 @@ class Employee:
             SET name = ?, job_title = ?, department_id = ?
             WHERE id = ?
         """
-        CURSOR.execute(sql, (self.name, self.job_title,
-                             self.department_id, self.id))
+        CURSOR.execute(sql, (self.name, self.job_title, self.department_id, self.id))
         CONN.commit()
 
     def delete(self):
         """Delete the table row corresponding to the current Employee instance,
         delete the dictionary entry, and reassign id attribute"""
-
         sql = """
             DELETE FROM employees
             WHERE id = ?
         """
-
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
-
-        # Delete the dictionary entry using id as the key
         del type(self).all[self.id]
-
-        # Set the id to None
         self.id = None
 
     @classmethod
@@ -134,16 +117,12 @@ class Employee:
     @classmethod
     def instance_from_db(cls, row):
         """Return an Employee object having the attribute values from the table row."""
-
-        # Check the dictionary for  existing instance using the row's primary key
         employee = cls.all.get(row[0])
         if employee:
-            # ensure attributes match row values in case local instance was modified
             employee.name = row[1]
             employee.job_title = row[2]
             employee.department_id = row[3]
         else:
-            # not in dictionary, create new instance and add to dictionary
             employee = cls(row[1], row[2], row[3])
             employee.id = row[0]
             cls.all[employee.id] = employee
@@ -153,23 +132,18 @@ class Employee:
     def get_all(cls):
         """Return a list containing one Employee object per table row"""
         sql = """
-            SELECT *
-            FROM employees
+            SELECT * FROM employees
         """
-
         rows = CURSOR.execute(sql).fetchall()
-
         return [cls.instance_from_db(row) for row in rows]
 
     @classmethod
     def find_by_id(cls, id):
         """Return Employee object corresponding to the table row matching the specified primary key"""
         sql = """
-            SELECT *
-            FROM employees
+            SELECT * FROM employees
             WHERE id = ?
         """
-
         row = CURSOR.execute(sql, (id,)).fetchone()
         return cls.instance_from_db(row) if row else None
 
@@ -177,14 +151,18 @@ class Employee:
     def find_by_name(cls, name):
         """Return Employee object corresponding to first table row matching specified name"""
         sql = """
-            SELECT *
-            FROM employees
-            WHERE name is ?
+            SELECT * FROM employees
+            WHERE name = ?
         """
-
         row = CURSOR.execute(sql, (name,)).fetchone()
         return cls.instance_from_db(row) if row else None
 
     def reviews(self):
         """Return list of reviews associated with current employee"""
-        pass
+        from review import Review  # Import here to avoid circular import issue
+        sql = """
+            SELECT * FROM reviews WHERE employee_id = ?
+        """
+        rows = CURSOR.execute(sql, (self.id,)).fetchall()
+        return [Review.instance_from_db(row) for row in rows]
+
